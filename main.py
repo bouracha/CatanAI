694f51a0a7413f3f38453d39da0c01391b7e5778
bouracha <bouracha@tcd.ie>
2016-07-24 01:14:36 +0100

    main.py
    Hex class definition and call to game setup

---
+++ main.py
@@ -0,0 +1,22 @@
+import itertools, random
+
+from GameSetup import setup
+
+class Hex(object):
+    def __init__(self, resource, value=(0, 0, 0), location=(0,0)):
+        self.resource = resource
+        self.value = value
+        self.location = location
+
+    def __str__(self):
+                return 'A "%s" hex, with number: %s, and value %s is at position: %s' % (self.resource, self.number, self.value, self.location)
+    def getResource(self):
+        return self.resource
+    def getValue(self):
+        return self.value
+    def getLocation(self):
+        return self.location
+
+if __name__=='__main__':
+
+    setup(Hex, itertools, random)
